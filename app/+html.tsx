e050c0d0aa9024588815247a56800ea04652f3d3
Pauline Odet <p.odet@niryo.com>
2026-01-10 11:45:12 +0100

    Deploy - test to fix app on ios

--- app/+html.tsx
+++ app/+html.tsx
@@ -11,10 +11,26 @@ export default function Root({ children }: PropsWithChildren) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover" />
 
+        {/* PWA Primary Tags */}
+        <meta name="application-name" content="SAP Festival" />
+        <meta name="description" content="SAP Festival - Official App" />
+        <meta name="theme-color" content="#5a9adb" />
+        
         {/* Link the PWA manifest file. */}
         <link rel="manifest" href="/manifest.json" />
+
+        {/* iOS Specific PWA Tags */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="SAP Festival" />
+        <link rel="apple-touch-icon" href="/logo192.png" />
+        <link rel="apple-touch-icon" sizes="192x192" href="/logo192.png" />
+        <link rel="apple-touch-icon" sizes="512x512" href="/logo512.png" />
+        
+        {/* Favicon */}
+        <link rel="icon" type="image/png" href="/logo192.png" />
 
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
